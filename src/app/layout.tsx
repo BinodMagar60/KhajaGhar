959e6f4fe679aaa2515ad346d5e24af1ee043381
BinodMagar60 <kauchabinod88@gmail.com>
2025-07-17 10:15:34 +0545

    support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {  Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/common/Navbar";
+import { Toaster } from "sonner";
 
 const inter = Inter({
   subsets: ["latin"]
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased bg-[#F6F7F8] min-h-screen w-full`}
       >
+        <Toaster position="bottom-right" />
         <Navbar/>
         {children}
       </body>
--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,5 +1,11 @@
 'use client'
 
+import { Phone, Mail } from "lucide-react"
+import { toast } from "sonner"
+const icons = {
+    Phone: Phone,
+    Mail: Mail,
+}
 
 const page = () => {
     return (
@@ -8,12 +14,81 @@ const page = () => {
                 <div className="text-2xl sm:text-3xl font-bold">Customer Support</div>
                 <div className="text-sm sm:text-base text-gray-600">We're here to help! Get in touch with us.</div>
             </div>
-            <div className="w-full">
-                <div className="w-full"></div>
-                <div></div>
+            <div className="w-full grid grid-cols-1 lg:grid-cols-3">
+                <div className="w-full lg:col-span-1">
+                    {
+                        [
+                            {
+                                title: "Call us",
+                                description: "Speak directly with our support team",
+                                icon: 'Phone',
+                                bottomTopPart: '9800000000',
+                                bottomBottomPart: "Sun-Fri: 8AM-10AM",
+                            },
+                            {
+                                title: "Emai Us",
+                                description: "Send us a detailed message",
+                                icon: "Mail",
+                                bottomTopPart: 'support@khajaghar.com',
+                                bottomBottomPart: "Response within 24 hours",
+                            },
+                        ].map(item => {
+                            const Icon = icons[item.icon as keyof typeof icons];
+                            return (
+                                <div key={item.title} className="border rounded-md border-gray-300 bg-white p-8 flex flex-col justify-center items-center cursor-pointer" onClick={() => {
+                                    navigator.clipboard.writeText(item.bottomTopPart)
+                                    toast.success(item.title === "Call us" ? "Contact Copied" : "Email Copied", {
+                                        duration: 1000
+                                    })
+                                }}>
+                                    <div><Icon className="w-8 h-8 text-[#F97316]" /></div>
+                                    <div className="font-semibold mt-4 mb-2">{item.title}</div>
+                                    <div className="text-gray-600 text-sm">{item.description}</div>
+                                    <div className="font-semibold mt-4">{item.bottomTopPart}</div>
+                                    <div className="text-gray-600 text-sm">{item.bottomBottomPart}</div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className="lg:col-span-2">
+
+                </div>
             </div>
         </div>
     )
 }
+
+
+function ContactFrom() {
+
+    return (
+        <form>
+            <div>
+                Send us a Message
+            </div>
+            <div>
+                <div>
+                    <div>Name*</div>
+                </div>
+                <div>
+                    <div>Emai*</div>
+                </div>
+            </div>
+            <div>
+                <div>Category*</div>
+            </div>
+            <div>
+                <div>Subject*</div>
+            </div>
+            <div>
+                <div>Message*</div>
+            </div>
+            <div></div>
+        </form>
+    )
+}
+
+
 
 export default page
